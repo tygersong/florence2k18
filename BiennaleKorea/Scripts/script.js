407c3d50e88571d3413f7f1d8f2a67e0c070a5ad
tygersong <36961343+tygersong@users.noreply.github.com>
2018-06-14 22:18:42 +0200

    Add files via upload

--- BiennaleKorea/Scripts/script.js
+++ BiennaleKorea/Scripts/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
 	$(".left").scrollTop(0);
 	$(".right").scrollTop(0);
+
+	$('.mainText').click(function(){
+		$(".left").scrollTop(0);
+		$(".right").scrollTop(0);
+	});
 });
